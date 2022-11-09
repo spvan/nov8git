440928d6553fa1c666143d400b90e67f82ae48b9
spvan <116981632+spvan@users.noreply.github.com>
2022-11-08 22:21:19 -0600

    initial commit

---
+++ nov8java/src/test/java/test/com/StudentTest.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package test.com;
+
+import com.itexps.utility.nov8java.Student;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Owner
+ */
+public class StudentTest {
+    Student stu;
+    
+    
+    public StudentTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        stu = new Student();
+        
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    @Test
+    public void testName(){
+        stu.setName("tom");
+        assertEquals("tom", stu.getName());
+        
+    }
+    
+}
